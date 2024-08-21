72e828e966a41ed8291c2c167f937fe959d2cadd
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-08-21 23:56:09 +0530

    initialise navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import { HamIcon, MenuIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="fixed left-0 top-0 w-10 bg-white h-screen flex flex-col justify-center items-center text-zinc-600 hover:text-zinc-950 delay-100">
+      <div>
+        <button>
+          <MenuIcon className="font-extrabold"/>
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
