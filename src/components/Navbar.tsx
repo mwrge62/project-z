4a0c09cfc78e51d500cb678948313ff90bafe360
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-08-23 19:03:42 +0530

    fix navbar ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+"use client";
 import {
   HomeIcon,
   ListVideo,
+  LogIn,
   LucideDownloadCloud,
   MenuIcon,
   MessageSquareDotIcon,
@@ -8,77 +10,124 @@ import {
   Settings,
 } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const [isPanelOpen, setPanelOpen] = useState(false);
+  const { data: session, status } = useSession();
+
   return (
-    <nav className="fixed left-0 top-0 w-[5rem] bg-zinc-800 h-[calc(100vh-1rem)] flex flex-col justify-between py-4 items-center text-zinc-400 m-2 rounded-2xl z-50">
-      <div className="flex flex-col gap-4 justify-center items-center">
-        <button className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75">
-          <MenuIcon strokeWidth={2.5} className="w-7 h-6" aria-hidden="true" />
+    <nav
+      className={`select-none fixed left-0 top-0 ${
+        isPanelOpen ? "w-60" : "w-20"
+      } bg-zinc-800 h-[calc(100vh-1rem)] flex flex-col justify-between py-4 ${
+        isPanelOpen ? "pl-4" : "items-center"
+      } text-zinc-400 m-2 rounded-2xl z-50 transition-all duration-75`}
+    >
+      <div className="flex flex-col gap-4">
+        <button
+          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 w-fit"
+          onClick={() => {
+            setPanelOpen((prev) => !prev);
+            console.log(`panel is ${!isPanelOpen}`);
+            console.log(session?.user);
+          }}
+        >
+          <MenuIcon strokeWidth={2.5} className="w-6 h-6" aria-hidden="true" />
         </button>
         <Link
           href="/"
-          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-col justify-center items-center"
+          className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+            isPanelOpen ? "justify-start gap-8 " : "justify-center"
+          } items-center`}
         >
           <HomeIcon strokeWidth={2.5} className="w-6 h-6" aria-hidden="true" />
-          <div className="text-xs">Home</div>
+          {isPanelOpen && <div className="text-xs ml-2">Home</div>}
         </Link>
         <Link
           href="/"
-          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-col justify-center items-center"
+          className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+            isPanelOpen ? "justify-start gap-8 " : "justify-center"
+          } items-center`}
         >
           <ListVideo strokeWidth={2.5} className="w-6 h-6" aria-hidden="true" />
-          <div className="text-xs">Watch</div>
+          {isPanelOpen && <div className="text-xs ml-2">Watch</div>}
         </Link>
         <Link
           href="/"
-          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-col justify-center items-center"
+          className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+            isPanelOpen ? "justify-start gap-8 " : "justify-center"
+          } items-center`}
         >
           <NotepadText
             strokeWidth={2.5}
             className="w-6 h-6"
             aria-hidden="true"
           />
-          <div className="text-xs">Read</div>
+          {isPanelOpen && <div className="text-xs ml-2">Read</div>}
         </Link>
         <Link
           href="/"
-          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-col justify-center items-center"
+          className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+            isPanelOpen ? "justify-start gap-8 " : "justify-center"
+          } items-center`}
         >
           <MessageSquareDotIcon
             strokeWidth={2.5}
             className="w-6 h-6"
             aria-hidden="true"
           />
-          <div className="text-xs">Engage</div>
+          {isPanelOpen && <div className="text-xs ml-2">Engage</div>}
         </Link>
         <Link
           href="/"
-          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-col justify-center items-center"
+          className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+            isPanelOpen ? "justify-start gap-8 " : "justify-center"
+          } items-center`}
         >
           <LucideDownloadCloud
             strokeWidth={2.5}
             className="w-6 h-6"
             aria-hidden="true"
           />
-          <div className="text-xs">Download</div>
+          {isPanelOpen && <div className="text-xs ml-2">Downloads</div>}
         </Link>
       </div>
-      <div className="flex flex-col gap-4 justify-center items-center">
-        <Link href="/profile">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </Link>
+      <div className="flex flex-col gap-4">
+        {session && session.user && session.user.image ? (
+          <Link
+            href="/profile"
+            className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+              isPanelOpen ? "justify-start gap-8 ml-[-8px]" : "justify-center"
+            } items-center`}
+          >
+            <Avatar className="">
+              <AvatarImage src={session.user.image} />
+              <AvatarFallback>Z</AvatarFallback>
+            </Avatar>
+            {isPanelOpen && <div className="text-xs">{session.user.name?.split(" ")[0]}</div>}
+          </Link>
+        ) : (
+          <Link
+            href="/"
+            className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+              isPanelOpen ? "justify-start gap-8" : "justify-center"
+            } items-center`}
+          >
+            <LogIn strokeWidth={2.5} className="w-6 h-6" aria-hidden="true" />
+            {isPanelOpen && <div className="text-xs">Log In</div>}
+          </Link>
+        )}
         <Link
           href="/settings"
-          className="p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-col justify-center items-center"
+          className={`p-2 rounded-[0.5rem] hover:text-zinc-50 transition-all duration-75 flex flex-row w-fit ${
+            isPanelOpen ? "justify-start gap-10 " : "justify-center"
+          } items-center`}
         >
           <Settings strokeWidth={2.5} className="w-6 h-6" aria-hidden="true" />
-          <div className="text-xs">Settings</div>
+          {isPanelOpen && <div className="text-xs">Settings</div>}
         </Link>
       </div>
     </nav>
